8a9aa1cff145c6cf83f6e86b9f70f323771adb86
amagkn <dmitrii.amagkn@gmail.com>
2024-10-07 10:38:34 +0300

    chore: Initial setup with Next.js and integrate design system nextui

---
+++ src/app/providers.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React, { PropsWithChildren } from 'react';
+import { NextUIProvider } from '@nextui-org/react';
+
+export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
+  return <NextUIProvider>{children}</NextUIProvider>;
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -14,6 +11,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
